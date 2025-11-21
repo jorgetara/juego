5fd1f59dbdf182f7a0d361499bd54f8c1a337960
jorgetara <jorgetaravilla0@gmail.com>
2025-11-21 14:03:17 +0100

    update

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,14 +1,42 @@
+import robot from "../assets/robot.svg"
 
 export default class Game extends Phaser.Scene {
     constructor() {
         super("Game")
     }
-    preload() { }
+    preload() { 
+        this.load.image('robot',robot)
+    }
     create() {
-        this.add.text(400,300, " pantalla", {
-            fontSize: "48px",
-            fill: "#efefef"
-        }).setOrigin(0.5)
+    
+        const walls = this.physics.add.staticGroup();
+        const mapa = [
+            "############",
+            "#..........#",
+            "#..#....#..#",
+            "#..##..##..#",
+            "#..#....#..#",
+            "#..........#",
+            "############"
+        ];
+        const tileW = this.scale.width / mapa[0].length;
+        const tileH = this.scale.height / mapa.length;
+        mapa.forEach((fila, y) => {
+            fila.split("").forEach((c, x) => {
+                if (c === "#") {
+                    const wall = this.add.rectangle(
+                        x * tileW + tileW / 2,
+                        y * tileH + tileH / 2,
+                        tileW,
+                        tileH,
+                        0xcccccc
+                    );
+                    this.physics.add.existing(wall, true);
+                    walls.add(wall);
+                }
+            });
+        });
+        this.robot=this.add.image(400,300,"robot").setScale(0.4)
         
     }
     update() { }
--- src/scenes/Portada.js
+++ src/scenes/Portada.js
@@ -1,10 +1,16 @@
+import efecto from "../assets/click.mp3"
+import ambiente from "../assets/arcade.mp3"
+
 export default class Portada extends Phaser.Scene {
     constructor() {
         super("Portada")
     }
-    preload() { }
+    preload() { 
+        this.load.audio('ambiente',ambiente)
+        this.load.audio('click',efecto)
+    }
     create() {
-        this.title = this.add.text(400,300, "Juego", {
+        this.title = this.add.text(400,150, "Juego", {
             fontSize: "64px",
             fill: "#efefef"
         }).setOrigin(0.5)
@@ -14,18 +20,32 @@ export default class Portada extends Phaser.Scene {
             this.scene.start("Game");
         })
 
-        //Hacemos Instrucciones
-        this.instrucciones = this.add.text(400,450, "Instrucciones", {
-            fontSize: "64px",
-            fill: "#ffff00"
-        }).setOrigin(0.5)
-        //Lanzamos el evento:
-        this.instrucciones.setInteractive();
-        this.instrucciones.on('pointerdown', () => {
-            this.scene.start("Instrucciones");
-        })
+        this.createUIButton(400, 300, "Start", () => this.scene.start("Game"))
+        this.createUIButton(400, 400, "Instrucciones", () => this.scene.start("Instrucciones"))
 
+        this.click=this.sound.add("click")
+        this.musica=this.sound.add("ambiente")
+        this.input.once("pointerdown",()=>{
+            this.musica.play()
+        })
        
+    }
+    createUIButton(x, y, label, callback) {
+        const width = 200;
+        const height = 60;
+        const bg = this.add.rectangle(x, y, width, height, 0xff0000, 1).setOrigin(0.5).setStrokeStyle(3,0xffffff).setInteractive()
+        const text = this.add.text(x, y, label, {
+            fontSize: "24px",
+            fill: "#FFFFFF"
+        }).setOrigin(0.5).setInteractive()
+        bg.on('pointerdown', () => {
+            this.click.play()
+            callback();
+        })
+        text.on('pointerdown', () => {
+            this.click.play()
+            callback();
+        })
     }
     update() { }
 }
